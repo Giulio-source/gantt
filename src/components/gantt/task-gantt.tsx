3683c66f57eb1fd55f2df28c5c3fb847eb23e6c1
Giulio Poggia <giulio.poggia@hinto.com>
2024-05-03 14:50:02 +0200

    first commit

---
+++ src/components/gantt/task-gantt.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useRef } from "react";
+import { Calendar, CalendarProps } from "../calendar/calendar";
+import { Grid, GridProps } from "../grid/grid";
+import styles from "./gantt.module.css";
+import { TaskGanttContent, TaskGanttContentProps } from "./task-gantt-content";
+
+export type TaskGanttProps = {
+  gridProps: GridProps;
+  calendarProps: CalendarProps;
+  barProps: TaskGanttContentProps;
+  ganttHeight: number;
+  scrollY: number;
+  scrollX: number;
+};
+export const TaskGantt: React.FC<TaskGanttProps> = ({
+  gridProps,
+  calendarProps,
+  barProps,
+  ganttHeight,
+  scrollY,
+  scrollX,
+}) => {
+  const ganttSVGRef = useRef<SVGSVGElement>(null);
+  const horizontalContainerRef = useRef<HTMLDivElement>(null);
+  const verticalGanttContainerRef = useRef<HTMLDivElement>(null);
+  const newBarProps = { ...barProps, svg: ganttSVGRef };
+  const rowsCount = barProps.tasks.every(
+    task => typeof task.overrideRowNumber === "number"
+  )
+    ? Math.max(...barProps.tasks.map(task => task.overrideRowNumber!)) + 1
+    : barProps.tasks.length;
+
+  useEffect(() => {
+    if (horizontalContainerRef.current) {
+      horizontalContainerRef.current.scrollTop = scrollY;
+    }
+  }, [scrollY]);
+
+  useEffect(() => {
+    if (verticalGanttContainerRef.current) {
+      verticalGanttContainerRef.current.scrollLeft = scrollX;
+    }
+  }, [scrollX]);
+
+  return (
+    <div
+      className={styles.ganttVerticalContainer}
+      ref={verticalGanttContainerRef}
+      dir="ltr"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={gridProps.svgWidth}
+        height={calendarProps.headerHeight}
+        fontFamily={barProps.fontFamily}
+      >
+        <Calendar {...calendarProps} />
+      </svg>
+      <div
+        ref={horizontalContainerRef}
+        className={styles.horizontalContainer}
+        style={
+          ganttHeight
+            ? { height: ganttHeight, width: gridProps.svgWidth }
+            : { width: gridProps.svgWidth }
+        }
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={gridProps.svgWidth}
+          height={barProps.rowHeight * rowsCount}
+          fontFamily={barProps.fontFamily}
+          ref={ganttSVGRef}
+        >
+          <Grid {...gridProps} />
+          <TaskGanttContent {...newBarProps} />
+        </svg>
+      </div>
+    </div>
+  );
+};
